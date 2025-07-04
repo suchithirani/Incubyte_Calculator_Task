e508f82951bb7b173bdc089e810719bb5bef0ae6
Suchit_Hirani001 <suchithirani@gmail.com>
2025-07-04 19:02:41 +0530

    for single string number done

--- Calculator/StringCalculator.java
+++ Calculator/StringCalculator.java
@@ -3,8 +3,12 @@ package Calculator;
 public class StringCalculator {
 
     public int add(String numbers){
-        if(numbers==null || numbers.isEmpty())
-            return 0;
+        if (!numbers.contains(",")) {
+            return Integer.parseInt(numbers.trim());
+        }
+
+        // For future use (like two numbers)
         return -1;
     }
+
 }
--- CalculatorTesting/StringCalculatorTest.java
+++ CalculatorTesting/StringCalculatorTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class StringCalculatorTest {
 
     @Test
-    public void testAddEmptyStringReturnsZero() {
+    public void testAddSingleNumber() {
         StringCalculator calc = new StringCalculator();
-        assertEquals(0, calc.add(""));
+        assertEquals(15, calc.add("15"));
     }
 }
